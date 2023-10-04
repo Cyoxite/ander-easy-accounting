6d04a528b8b4cb17d9fef3804b1f4b3a529e43bc
Cyoxite <ander.polanski@gmail.com>
2023-10-04 15:32:26 +0200

    first commit.

---
+++ ander-easy-accounting.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+     * Plugin Name: Ander Easy Accounting
+     * Description: Easy accounting for you or your company
+     * Author: Daniel Ander Polański
+     * Version: 1.0.0
+     * Requires PHP: 7.0
+     * Requires at least: 5.6
+     * Tested up to: 6.3
+     * Text Domain: ander-easy-accounting
+     * Author URI: https://e-ander.pl
+     */
+
+ defined( 'ABSPATH' ) or die( "Oh, It's bad way..." );
